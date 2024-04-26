ae2e394eb5df75778703758b87ff05919d574386
Nethpali_Jayaweera <nethpalijayaweera@gmail.com>
2024-04-26 11:05:50 +0530

    Update Supplier

--- src/main/java/lk/com/ijse/hello_shoes_test/Controller/SupplierController.java
+++ src/main/java/lk/com/ijse/hello_shoes_test/Controller/SupplierController.java
@@ -3,6 +3,7 @@ package lk.com.ijse.hello_shoes_test.Controller;
 import lk.com.ijse.hello_shoes_test.DTO.SupplierDto;
 import lk.com.ijse.hello_shoes_test.Service.SupplierService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -23,5 +24,11 @@ public class SupplierController {
     public SupplierDto saveSupplier(@RequestBody SupplierDto supplierDto){
         supplierDto.setSupplier_code(UUID.randomUUID().toString());
         return supplierService.saveSupplier(supplierDto);
+    }
+
+    @PutMapping(value="/update",consumes= MediaType.APPLICATION_JSON_VALUE)
+    public boolean updateSupplier(@RequestBody SupplierDto supplierDto){
+        return supplierService.updateSupplier(supplierDto.getSupplier_code(), supplierDto);
+
     }
 }
--- src/main/java/lk/com/ijse/hello_shoes_test/Service/Impl/SupplierServiceIMPL.java
+++ src/main/java/lk/com/ijse/hello_shoes_test/Service/Impl/SupplierServiceIMPL.java
@@ -4,12 +4,16 @@ import jakarta.transaction.Transactional;
 import lk.com.ijse.hello_shoes_test.DTO.SupplierDto;
 import lk.com.ijse.hello_shoes_test.Dao.CustomerDao;
 import lk.com.ijse.hello_shoes_test.Dao.SupplierDao;
+import lk.com.ijse.hello_shoes_test.Entity.Customer;
+import lk.com.ijse.hello_shoes_test.Entity.Supplier;
 import lk.com.ijse.hello_shoes_test.Service.SupplierService;
 import lk.com.ijse.hello_shoes_test.Util.Mapping;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -23,6 +27,23 @@ public class SupplierServiceIMPL implements SupplierService {
 
     @Override
     public boolean updateSupplier(String id, SupplierDto supplierDto) {
+        Optional<Supplier> supplier = supplierDao.findById(id);
+        if (supplier.isPresent()) {
+
+            supplier.get().setSupplier_name(supplierDto.getSupplier_name());
+            supplier.get().setCategory(supplierDto.getCategory());
+            supplier.get().setAddress_01(supplierDto.getAddress_01());
+            supplier.get().setAddress_02(supplierDto.getAddress_02());
+            supplier.get().setAddress_03(supplierDto.getAddress_03());
+            supplier.get().setAddress_04(supplierDto.getAddress_04());
+            supplier.get().setAddress_05(supplierDto.getAddress_05());
+            supplier.get().setAddress_06(supplierDto.getAddress_06());
+            supplier.get().setContact_no_01(supplierDto.getContact_no_01());
+            supplier.get().setContact_no_02(supplierDto.getContact_no_02());
+            supplier.get().setEmail(supplierDto.getEmail());
+
+            return true;
+        }
         return false;
     }
 
