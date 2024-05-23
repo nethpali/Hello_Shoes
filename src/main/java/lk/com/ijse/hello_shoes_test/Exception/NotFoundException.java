f07959bc31db5c5260c7004da2af61a45dcb812f
Nethpali_Jayaweera <nethpalijayaweera@gmail.com>
2024-05-23 22:47:33 +0530

    Exception Class Done

---
+++ src/main/java/lk/com/ijse/hello_shoes_test/Exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package lk.com.ijse.hello_shoes_test.Exception;
+
+public class NotFoundException extends Exception{
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
